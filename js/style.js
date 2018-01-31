99e536ddbb7dc093d3fbb67fcbf186443c29a2ec
Ryan Karumanchery <ryankarumanchery@gmail.com>
2018-01-30 20:22:19 -0500

    Temporarily Removed Javascript functionality

--- js/style.js
+++ js/style.js
@@ -2,13 +2,29 @@ var menu = {
 
     init: function () {
 
-        var pageN = "dos";
+        var pageN = ['uno', 'dos', 'tres', 'cuatro']
 
         function enlarge(id) {
             document.getElementById(id).style.color = "red";
         }
 
-        document.getElementById(pageN).addEventListener("mouseover", enlarge(pageN));
+        for (i = 0; i <= pageN.length; i++) {
+
+            var currentPage = pageN[i];
+            var element = document.getElementById(currentPage)
+
+            element.addEventListener("mouseover", function () {
+                if (this.getAttribute("color") == "red") {
+                    this.style.color = "grey";
+                } else {
+                    this.style.color = "red";
+                }
+            });
+
+
+
+        }
+
 
     }
 }
